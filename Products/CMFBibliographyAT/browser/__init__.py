df0d905c97cc4543b3c5e8fb8a1d744807e0e523
tom_gross <tom_gross@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-27 14:33:14 +0000

    make "export"-action reappear on bibliography-folders

--- Products/CMFBibliographyAT/browser/__init__.py
+++ Products/CMFBibliographyAT/browser/__init__.py
@@ -1,1 +1,8 @@
 # Init of browser package
+
+from bibliograph.core.interfaces import IBibliographyExport
+
+class isBibliographyExportable(object):
+    
+    def __call__(self):
+        return IBibliographyExport.providedBy(self.context)

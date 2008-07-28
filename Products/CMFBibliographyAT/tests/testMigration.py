135fa8b42f505d94f9b06473c2c866a3b87a656d
pbugni <pbugni@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-07-28 23:33:12 +0000

    Extended migration-script to also remove persistent 'Parsers'

--- Products/CMFBibliographyAT/tests/testMigration.py
+++ Products/CMFBibliographyAT/tests/testMigration.py
@@ -13,18 +13,31 @@ class TestMigration09to10(ptc.PloneTestCase):
     def test_toolmigration(self):
         out = StringIO()
         makeContent(self.folder, portal_type='Folder', id='Renderers')
+        makeContent(self.folder, portal_type='Folder', id='Parsers')
 
         assert 'Renderers' in self.folder.objectIds()
+        assert 'Parsers' in self.folder.objectIds()
 
         migration = Migration(self.portal, out)
         migration.migrateTool(self.folder)
 
         self.failIf('Renderers' in self.folder.objectIds())
+        self.failIf('Parsers' in self.folder.objectIds())
 
         # run again to make sure, it's safe.
         migration.migrateTool(self.folder)
 
         self.failIf('Renderers' in self.folder.objectIds())
+        self.failIf('Parsers' in self.folder.objectIds())
+
+        # run again with only Parsers in place
+        makeContent(self.folder, portal_type='Folder', id='Parsers')
+
+        assert 'Parsers' in self.folder.objectIds()
+
+        migration.migrateTool(self.folder)
+
+        self.failIf('Parsers' in self.folder.objectIds())
 
 def test_suite():
     from unittest import TestSuite, makeSuite

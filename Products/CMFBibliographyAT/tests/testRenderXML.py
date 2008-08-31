75610b1f0d27f1e209cb6ae2b571359cef6c7014
tim2p <tim2p@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-08-31 00:49:53 +0000

    Moving the _getCommand/_hasCommands implementation from bibliograph.rendering to bibliograph.core so that bibliograph.parsing doesn't have an artificial dependency on bibliograph.rendering.

--- Products/CMFBibliographyAT/tests/testRenderXML.py
+++ Products/CMFBibliographyAT/tests/testRenderXML.py
@@ -16,7 +16,7 @@ from Testing import ZopeTestCase
 from Products.CMFPlone.tests import PloneTestCase
 from Products.CMFCore.utils import getToolByName
 
-from bibliograph.rendering.utility import _getCommand
+from bibliograph.core.bibutils import _getCommand
 
 from Products.CMFBibliographyAT.tests import setup
 from Products.CMFBibliographyAT import testing

f9f124a087619dae86b2f4f7fe5a8a5770e7e243
ajung <ajung@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-10 09:02:12 +0000

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.0a7'
+version = '1.0.0b1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()

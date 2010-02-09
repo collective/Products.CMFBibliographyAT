d929b619ae7134858f475174a492bf939a043e7a
ajung <ajung@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-09 05:42:33 +0000

    added comment

--- Products/CMFBibliographyAT/browser/__init__.py
+++ Products/CMFBibliographyAT/browser/__init__.py
@@ -6,4 +6,7 @@ from Products.CMFBibliographyAT.interface import IBibliographicItem
 class isBibliographyExportable(object):
     
     def __call__(self):
+        # This check is completely bullshit: it does not take export
+        # adapters into account nor does it work with the old-style
+        # IBibliographyExport interface as used in base.py
         return IBibliographyExport.providedBy(self.context) or IBibliographicItem.providedBy(self.context)

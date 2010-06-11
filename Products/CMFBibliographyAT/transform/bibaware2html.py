888cb6134799f34c4b0787b1b3982577f10c6212
ajung <ajung@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-06-11 12:10:32 +0000

    fix

--- Products/CMFBibliographyAT/transform/bibaware2html.py
+++ Products/CMFBibliographyAT/transform/bibaware2html.py
@@ -3,7 +3,7 @@ import pkg_resources
 dist = pkg_resources.get_distribution('Plone')
 if dist.version.startswith('3'):
     import bibaware2html_plone3
-    from bibaware2html_plone3 import HTMLToBibaware, register, initialize
+    from bibaware2html_plone3 import BibawareToHTML, register, initialize
 else:
     import bibaware2html_plone4
-    from bibaware2html_plone4 import HTMLToBibaware, register, initialize
+    from bibaware2html_plone4 import BibawareToHTML, register, initialize

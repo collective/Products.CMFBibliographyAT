e354c3a0c2470b9280f29c8d8bc5ac43e322ec7f
jensens <jensens@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-11-01 14:41:37 +0000

    chnages

--- Products/CMFBibliographyAT/browser/import.py
+++ Products/CMFBibliographyAT/browser/import.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFCore.utils import getToolByName 
+from Products.CMFCore.utils import getToolByName
 from Products.Archetypes import PloneMessageFactory as _
 from Products.Archetypes.utils import addStatusMessage
 from bibliograph.core.utils import _encode
@@ -11,14 +11,14 @@ logger = logging.getLogger('CMFBibliographyAT import')
 
 
 class ImportView(BrowserView):
-    
+
     template = ViewPageTemplateFile('import.pt')
-    
-    def __call__(self):       
+
+    def __call__(self):
         self.processed = False
         self.errors = {}
         start_time = self.context.ZopeTime().timeTime()
-        if not self.request.form.get('form.submitted'): 
+        if not self.request.form.get('form.submitted'):
             return self.template()
 
         # fetch value from request
@@ -44,93 +44,95 @@ class ImportView(BrowserView):
                 self.errors['file'] = msg
                 addStatusMessage(self.request, _(unicode(msg)))
                 return self.template()
-        
+
         # skip DOS line breaks
         source = source.replace('\r', '')
-        
+
         # get parsed entries from the Bibliography Tool
         bibtool = getToolByName(self.context, 'portal_bibliography')
         try:
-            entries = bibtool.getEntries(source, format, filename, 
+            entries = bibtool.getEntries(source, format, filename,
                                          input_encoding=input_encoding)
         except ImportParseError:
             msg = """%s Parser's 'checkFormat' and guessing the format""" \
-                  """ from the file name '%s' failed.""" % (format, 
+                  """ from the file name '%s' failed.""" % (format,
                                                             filename)
             self.errors['format'] = msg
-            addStatusMessage(self.request,_(unicode(msg)))
+            addStatusMessage(self.request, _(unicode(msg)))
             return self.template()
         except UnicodeError:
             msg = """The choosen input encoding does not match the real  """ \
                   """encoding of your input data in order to convert it to """\
                   """unicode internally."""
             self.errors['input_encoding'] = msg
-            addStatusMessage(self.request,_(unicode(msg)))
-            return self.template()        
-        except RuntimeError,e:
-            addStatusMessage(self.request,_(unicode(e)))
+            addStatusMessage(self.request, _(unicode(msg)))
             return self.template()
-        
+        except RuntimeError, e:
+            addStatusMessage(self.request, _(unicode(e)))
+            return self.template()
+
         # debug message if entries is not a python list
         if not entries or not isinstance(entries, (list, tuple)):
             msg = "There must be something wrong with the parser"
-            addStatusMessage(self.request,_(unicode(msg)))
+            addStatusMessage(self.request, _(unicode(msg)))
             return self.template()
-        
+
         # start building the report
-        mtool  = getToolByName(self.context, 'portal_membership')
+        mtool = getToolByName(self.context, 'portal_membership')
         member = mtool.getAuthenticatedMember()
         fullname = member.getProperty('fullname', None)
         if fullname:
             username = '%s (%s)' % (_encode(fullname), _encode(member.getId()))
         else:
-            username = _encode(member.getId())          
-        tmp_report = '[%s] Imported by %s' % (self.context.ZopeTime(), username)
+            username = _encode(member.getId())
+        tmp_report = '[%s] Imported by %s' % (self.context.ZopeTime(),
+                                              username)
         if filename is not None:
             tmp_report += ' from file %s' % _encode(filename)
-        tmp_report += ':\n\n' 
-        
+        tmp_report += ':\n\n'
+
         # process import for each entry
         processedEntries = 0
         importErrors = 0
 
         logger.info('Start import of %s raw entries.' % len(entries))
-        counter = 0     
-                
+        counter = 0
+
         for entry in entries:
             counter += 1
-            count = '#%05i: ' % counter 
-            logger.info(count+'processing entry')
+            count = '#%05i: ' % counter
+            logger.info(count + 'processing entry')
             # Workaround for #36 where an entry represents
             # an error from parser instead of a dict containing
             # importable data
             if isinstance(entry, basestring):
                 msg = 'Entry could not be parsed! %s' % _encode(entry)
                 upload = (msg, 'error')
-                logger.error(count+msg)   
+                logger.error(count + msg)
             elif entry.get('title'):
-                logger.info(count+'Normal processing')   
+                logger.info(count + 'Normal processing')
                 upload = self.context.processSingleImport(entry,
-                                                  span_of_search=span_of_search)
+                                                 span_of_search=span_of_search)
             else:
-                formated = '; '.join(['%s=%s' % (key, entry[key]) 
-                                      for key in sorted(entry.keys()) 
+                formated = '; '.join(['%s=%s' % (key, entry[key])
+                                      for key in sorted(entry.keys())
                                       if key == key.lower()])
-                upload = ('Found entry without title: %s\n' % formated, 
+                upload = ('Found entry without title: %s\n' % formated,
                           'error')
-                logger.error(count+upload[0])   
+                logger.error(count + upload[0])
             if upload[1] == 'ok':
                 processedEntries += 1
             else:
                 importErrors += 1
             state, msg = _encode(upload[1].upper()), _encode(upload[0])
-            tmp_report += '%s: %s\n' % (state, msg)        
+            tmp_report += '%s: %s\n' % (state, msg)
         self.context.logImportReport(tmp_report)
-        self.processed = True        
+        self.processed = True
         # set the portal status message up
-        msg = "Processed %i entries. There were %i errors. Import processed in %f seconds. See import report below." \
-              % (processedEntries, importErrors, 
-                 self.context.ZopeTime().timeTime()-start_time)
+        msg = "Processed %i entries. There were %i errors. "\
+              "Import processed in %f seconds. See import report below." \
+              % (processedEntries, importErrors,
+                 self.context.ZopeTime().timeTime() - start_time)
         logger.info(msg)
         addStatusMessage(self.request, _(unicode(msg)))
-        return self.template() 
+        return self.template()
